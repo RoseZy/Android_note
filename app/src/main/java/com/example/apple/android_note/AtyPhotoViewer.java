fc898f11d13766f256a9612c5b5a640e8ca3e226
apple <963182316@qq.com>
2017-05-31 10:18:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/apple/android_note/AtyPhotoViewer.java
@@ -0,0 +1,41 @@
+package com.example.apple.android_note;
+
+/**
+ * Created by 张越 on 17/5/4.
+ */
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.File;
+
+/**
+ * 显示照片的Activity
+ *
+ * @author TOPS
+ *
+ */
+public class AtyPhotoViewer extends Activity {
+
+    private ImageView iv;
+
+    public static final String EXTRA_PATH = "path";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        iv = new ImageView(this);
+        setContentView(iv);
+
+        String path = getIntent().getStringExtra(EXTRA_PATH);
+        if (path != null) {
+            iv.setImageURI(Uri.fromFile(new File(path)));
+        } else {
+            finish();
+        }
+    }
+
+}
